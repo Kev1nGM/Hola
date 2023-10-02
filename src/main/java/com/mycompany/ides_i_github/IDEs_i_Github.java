c5f56acfa927936c62131192ed96f0772232cf6d
Kevin <Kevin@DESKTOP-3T7T495.home>
2023-10-02 21:48:00 +0200

    Primer commit, Hola Kevin

---
+++ src/main/java/com/mycompany/ides_i_github/IDEs_i_Github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ides_i_github;
+
+/**
+ *
+ * @author Kevin
+ */
+public class IDEs_i_Github {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Kevin");
+    }
+}
